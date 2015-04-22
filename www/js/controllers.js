810f0c575ccf5b76989d89498c9d1dd951454f98
Fengda HUANG <h@phodal.com>
2015-04-22 10:23:20 +0800

    move map to controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,51 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+	var exampleNS = {};
+
+	exampleNS.getRendererFromQueryString = function() {
+		var obj = {}, queryString = location.search.slice(1),
+			re = /([^&=]+)=([^&]*)/g, m;
+
+		while (m = re.exec(queryString)) {
+			obj[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+		}
+		if ('renderers' in obj) {
+			return obj['renderers'].split(',');
+		} else if ('renderer' in obj) {
+			return [obj['renderer']];
+		} else {
+			return undefined;
+		}
+	};
+
+	var map = new ol.Map({
+		layers: [
+			new ol.layer.Tile({
+				source: new ol.source.OSM()
+			})
+		],
+		renderer: exampleNS.getRendererFromQueryString(),
+		target: 'map',
+		controls: ol.control.defaults({
+			attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+				collapsible: false
+			})
+		}),
+		view: new ol.View({
+			center: [0, 0],
+			zoom: 2
+		})
+	});
+
+	jQuery('#map').after('<button type="button" ' +
+	'onclick="map.getView().setZoom(map.getView().getZoom() - 1);">' +
+	'Zoom out</button>');
+	jQuery('#map').after('<button type="button" ' +
+	'onclick="map.getView().setZoom(map.getView().getZoom() + 1);">' +
+	'Zoom in</button>');
+
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   $scope.chats = Chats.all();
