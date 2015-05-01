fd7686dad9fbc662bf2c402c3921798cb1fd7cd9
Fengda HUANG <h@phodal.com>
2015-05-01 23:16:11 +0800

    Revert "use span replace a"
    This reverts commit e54f3973dfa28fa10a4ce8978bb9d81e701e14cb.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -100,7 +100,7 @@ angular.module('starter.controllers', ['ngCordova', 'elasticsearch'])
 					'html': true,
 					'content': "<h4>商品:" + feature.get('name') + "</h4>" + '' +
 					'<div class="button icon-left ion-ios-telephone button-calm button-outline">' +
-					'<span ng-href="tel: {{result.phone_number}}">' + feature.get('phone') + '</span> </div>' +
+					'<a ng-href="tel: {{result.phone_number}}">' + feature.get('phone') + '</a> </div>' +
 						"<p class='icon-left ion-ios-navigate'> " + feature.get('distance') + "公里</p>"
 				});
 				$(element).popover('show');
