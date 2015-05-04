a9d90f5dca17c8c3e40e2e47b69280daf5ab652e
Fengda HUANG <h@phodal.com>
2015-05-04 08:49:14 +0800

    set tab default to lists

--- www/js/app.js
+++ www/js/app.js
@@ -69,7 +69,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/map');
+  $urlRouterProvider.otherwise('/tab/lists');
 
 });
